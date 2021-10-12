488c20d3924f8dfba105cf3fa4af9c6fc270d723
Txldarkrider <txldarkrider@gmail.com>
2021-10-12 19:35:33 -0400

    Update main.js

--- Pages/Javascript/WebsocketTest/main.js
+++ Pages/Javascript/WebsocketTest/main.js
@@ -1,4 +1,5 @@
-let socket = new WebSocket('wss://76.190.183.136:3000');
+// let socket = new WebSocket('wss://76.190.183.136:3000');
+let socket = new WebSocket('wss://192.168.0.18:3000');
 
 socket.addEventListener('open',(e)=>{
   socket.send('Hello Server!');
